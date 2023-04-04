5d18bad00eb5578b48f65ac5490e288259bb54c7
Florencia Vela <flor.p.vela@gmail.com>
2023-04-03 21:09:54 -0400

    C0 - hackathon

--- chapter 4/4_0.py
+++
@@ -1,8 +0,0 @@
-# count the number of bits set to 1 in a non-negative integer
-
-def count_bits(input_int: int) -> int:
-    num_bits = 0
-    while input_int:
-        num_bits += input_int & 1
-        input_int = input_int >> 1
-    return num_bits
